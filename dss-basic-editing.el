252bd617571ac95861e6a1fe638255e10133d447
Tavis Rudd <tavis@damnsimple.com>
2010-06-30 17:54:43 -0700

    visible-mark, goto-last-change and highlight-changes

--- dss-basic-editing.el
+++ dss-basic-editing.el
@@ -17,4 +17,26 @@
   (interactive "r")
   (dss/quote-region start end "'"))
 
+(require 'misc)      ; forward-to-word & backward-to-word
+
+(require 'visible-mark)
+(global-visible-mark-mode t)
+
+(require 'goto-last-change)             ;there is another version of this library called goto-chg.el
+
+;; see
+;; http://nicolas-lara.blogspot.com/2009/11/emacs-mark-stack.html
+;; http://www.bloomington.in.us/~brutt/marker-visit.el
+
+;;; http://emacs-fu.blogspot.com/2009/05/tracking-changes.html
+(setq highlight-changes-visibility-initial-state nil)
+(global-highlight-changes-mode t)
+;; (global-set-key (kbd "<f6>")      'highlight-changes-visible-mode) ;; changes
+;; ;; remove the change-highlight in region
+;; (global-set-key (kbd "S-<f6>")    'highlight-changes-remove-highlight)
+;; (global-set-key (kbd "<M-prior>") 'highlight-changes-next-change)
+;; (global-set-key (kbd "<M-next>")  'highlight-changes-previous-change)
+
+
+
 (provide 'dss-basic-editing)
