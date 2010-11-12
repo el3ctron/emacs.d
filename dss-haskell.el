f46670f628251ddd7766864144ca5fe7a2a21269
Tavis Rudd <tavis@damnsimple.com>
2010-11-11 19:34:14 -0800

    miscellaneous additions

--- dss-haskell.el
+++ dss-haskell.el
@@ -5,4 +5,33 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
+(add-to-list 'load-path "/home/tavis/.cabal/share/scion-current/emacs")
+(require 'scion)
+
+(add-to-list 'load-path "/home/tavis/.cabal/share/ghc-mod-current")
+(require 'ghc)
+
+(add-to-list 'load-path "/home/tavis/.cabal/share/hlint-current")
+(require 'hs-lint)
+
+;; if ./cabal/bin is not in your $PATH
+(setq scion-program "~/.cabal/bin/scion-server")
+
+;;; see http://www.emacswiki.org/cgi-bin/wiki/FlymakeHaskell
+
+(defun dss/haskell-hook ()
+  ;; (scion-mode 1)
+  ;; (scion-flycheck-on-save 1)
+  (linum-mode 1)
+  (dss/install-whitespace-cleanup-hook)
+  (dss/load-lineker-mode))
+
+(add-hook 'haskell-mode-hook 'dss/haskell-hook)
+
+;; Use ido-mode completion (matches anywhere, not just beginning)
+;;
+;; WARNING: This causes some versions of Emacs to fail so badly
+;; that Emacs needs to be restarted.
+(setq scion-completing-read-function 'ido-completing-read)
+
 (provide 'dss-haskell)
