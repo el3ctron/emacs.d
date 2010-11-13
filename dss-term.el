da8260d2c64afd47694c3591c5bbbe2b2b81449c
Tavis Rudd <tavis@damnsimple.com>
2010-11-12 18:45:25 -0800

    added support for GNU screen sessions to dss/term-setup-tramp

--- dss-term.el
+++ dss-term.el
@@ -63,12 +63,18 @@
   (interactive)
   (if (term-in-line-mode)
       (dss/term-char-mode)
-    (term-line-mode)))
+    (dss/term-line-mode)))
 
 (defun dss/term-char-mode ()
   (interactive)
   (term-char-mode)
+  (linum-mode -1)
   (comint-goto-process-mark))
+
+(defun dss/term-line-mode ()
+  (interactive)
+  (term-line-mode)
+  (linum-mode 1))
 
 (defun dss/term-dabbrev ()
   (interactive)
@@ -118,52 +124,73 @@
 (defun dss/term-setup-tramp ()
   "Setup ansi-term/tramp remote directory tracking"
   (interactive)
-  (term-send-raw-string "
-# Emacs ansi-term/tramp remote directory tracking
-if [ $TERM = eterm-color ]; then
-    function eterm-set-cwd {
-        $@
+  (term-send-raw-string
+   (concat "
+function eterm-set-variables {
+    local emacs_host=\"" (car (split-string (system-name) "\\.")) "\"
+    if [[ $TERM == \"eterm-color\" ]]; then
         echo -e \"\\033AnSiTc\" $(pwd)
-    }
-
-    # set hostname, user, and cwd
-    function eterm-reset {
-        echo -e \"\\033AnSiTu\" ${TRAMP_USERNAME-$(whoami)}
-        echo -e \"\\033AnSiTc\" $(pwd)
-        echo -e \"\\033AnSiTh\" ${TRAMP_HOSTNAME-$(hostname)}
-    }
-
+        if [[ ${HOSTNAME-$(hostname)} != \"$emacs_host\" ]]; then
+            echo -e \"\\033AnSiTu\" ${TRAMP_USERNAME-$(whoami)}
+            echo -e \"\\033AnSiTh\" ${TRAMP_HOSTNAME-$(hostname)}
+        fi
+    elif [[ $TERM == \"screen\" || $TERM  == \"screen-256color\" ]]; then
+        echo -e \"\\033P\\033AnSiTc\\033\\\\\" $(pwd)
+        if [[ ${HOSTNAME-$(hostname)} != \"$emacs_host\" ]]; then
+            echo -e \"\\033P\\033AnSiTu\\033\\\\\" ${TRAMP_USERNAME-$(whoami)}
+            echo -e \"\\033P\\033AnSiTh\\033\\\\\" ${TRAMP_HOSTNAME-$(hostname)}
+        fi
+    fi
+}
+function eterm-tramp-init {
     for temp in cd pushd popd; do
         alias $temp=\"eterm-set-cwd $temp\"
     done
 
     # set hostname, user, and cwd now
-    eterm-reset
-fi
-"))
+    eterm-set-variables
+}
+function eterm-set-cwd {
+    $@
+    eterm-set-variables
+}
+eterm-tramp-init
+export -f eterm-tramp-init
+export -f eterm-set-variables
+export -f eterm-set-cwd
+
+")))
 
 (setq term-bind-key-alist
-   '(("C-c C-c" . term-interrupt-subjob)
-     ("C-x C-x" . term-send-raw)
-     ;("C-p" . term-send-raw);previous-line)
-     ;("C-n" . term-send-raw);next-line)
-     ("C-s" . isearch-forward)
-     ("C-r" . dss/term-reverse-search)
-     ("C-m" . term-send-raw)
-     ("M-f" . term-send-forward-word)
-     ("M-b" . term-send-backward-word)
-     ("M-o" . term-send-backspace)
-     ("M-p" . term-send-up)
-     ("M-n" . term-send-down)
-     ("M-N" . term-send-backward-kill-word)
-     ("M-r" . term-send-reverse-search-history)
-     ("M-," . term-send-input)
-     ("M-." . comint-dynamic-complete)
-     ("Od" . term-send-backward-word)
-     ("Oc" . term-send-forward-word)
-     ("M-d" . term-send-forward-kill-word)
-     ("M-g" . dss/term-toggle-mode)
-     ("C-y" . dss/term-yank)))
+      '(("C-c C-c" . term-interrupt-subjob)
+        ("C-x C-x" . term-send-raw)
+        ("C-x C-e" . (lambda ()
+                       (interactive)
+                       (term-send-raw-string "\C-x\C-e")))
+                                        ;("C-p" . term-send-raw);previous-line)
+                                        ;("C-n" . term-send-raw);next-line)
+        ("C-s" . isearch-forward)
+        ("C-r" . dss/term-reverse-search)
+        ("C-m" . term-send-raw)
+
+        ("M-k" . term-send-raw-meta)
+        ("M-y" . term-send-raw-meta)
+        ("M-u" . term-send-raw-meta)
+
+        ("M-f" . term-send-forward-word)
+        ("M-b" . term-send-backward-word)
+        ("M-o" . term-send-backspace)
+        ("M-p" . term-send-up)
+        ("M-n" . term-send-down)
+        ("M-N" . term-send-backward-kill-word)
+        ("M-r" . term-send-reverse-search-history)
+        ("M-," . term-send-input)
+        ("M-." . comint-dynamic-complete)
+        ("Od" . term-send-backward-word)
+        ("Oc" . term-send-forward-word)
+        ("M-d" . term-send-forward-kill-word)
+        ("M-g" . dss/term-toggle-mode)
+        ("C-y" . dss/term-yank)))
 
 ;; also see http://dea.googlecode.com/svn/trunk/my-lisps/multi-term-settings.el
 
