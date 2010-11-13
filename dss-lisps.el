c612f61cfd48d744d209a49c86012e4a849c75e2
Tavis Rudd <tavis@damnsimple.com>
2010-11-12 18:43:31 -0800

    flymake-elisp-init, but commented out

--- dss-lisps.el
+++ dss-lisps.el
@@ -98,6 +98,34 @@
 
 (define-key emacs-lisp-mode-map (kbd "M-.") 'dss/find-function-at-point)
 
+;; ;; http://www.emacswiki.org/emacs/FlymakeElisp
+;; (defun flymake-elisp-init ()
+;;   (if (string-match "^ " (buffer-name))
+;;       nil
+;;     (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+;;                          'flymake-create-temp-inplace))
+;;            (local-file  (file-relative-name
+;;                          temp-file
+;;                          (file-name-directory buffer-file-name))))
+;;       (list
+;;        (expand-file-name invocation-name invocation-directory)
+;;        (list
+;;         "-Q" "--batch" "--eval"
+;;         (prin1-to-string
+;;          (quote
+;;           (dolist (file command-line-args-left)
+;;             (with-temp-buffer
+;;               (insert-file-contents file)
+;;               (condition-case data
+;;                   (scan-sexps (point-min) (point-max))
+;;                 (scan-error
+;;                  (goto-char (nth 2 data))
+;;                  (princ (format "%setq:%setq: error: Unmatched bracket or quote\n"
+;;                                 file (line-number-at-pos)))))))))
+;;         local-file)))))
+;; (push '("\\.el$" flymake-elisp-init) flymake-allowed-file-name-masks)
+;; (add-hook 'emacs-lisp-mode-hook 'flymake-mode)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; real lisp hackers use the lambda character
 ;; courtesy of stefan monnier on c.l.l
