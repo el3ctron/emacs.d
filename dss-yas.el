a98c2bef2434b480b0ab41919db0cdace3f875c3
Tavis Rudd <tavis@damnsimple.com>
2010-11-12 18:46:13 -0800

    (yas/load-directory dss-yas-snippets-dir)

--- dss-yas.el
+++ dss-yas.el
@@ -5,5 +5,6 @@
 (setq yas/trigger-key (kbd "C-c <tab>"))
 (yas/initialize)
 (yas/load-directory (concat dss-vendor-dir "yasnippet/snippets"))
+(yas/load-directory dss-yas-snippets-dir)
 
 (provide 'dss-yas)
