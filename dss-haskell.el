9fe7cb125670165966c371247cb6f47b92826f62
Tavis Rudd <tavis@damnsimple.com>
2010-12-15 14:26:44 -0800

    tweak the electric pairs

--- dss-haskell.el
+++ dss-haskell.el
@@ -19,6 +19,12 @@
 
 ;;; see http://www.emacswiki.org/cgi-bin/wiki/FlymakeHaskell
 
+(defun dss/haskell-electric-pair ()
+  ;; this version doesn't check to see if we're inside of a string or comment
+  (interactive)
+  (let (parens-require-spaces)
+    (insert-pair)))
+
 (defun dss/haskell-hook ()
   ;; (scion-mode 1)
   ;; (scion-flycheck-on-save 1)
@@ -26,18 +32,14 @@
   (dss/install-whitespace-cleanup-hook)
   (dss/load-lineker-mode)
   (mapc (lambda (char)
-            (progn
-              (define-key haskell-mode-map char 'dss/electric-pair)
-              (define-key inferior-haskell-mode-map char 'dss/electric-pair)
-              ))
-          '("\"" "(" "[" "{")))
+          (progn
+            (define-key haskell-mode-map char 'dss/haskell-electric-pair)
+            (define-key inferior-haskell-mode-map char 'dss/haskell-electric-pair)
+            ))
+        '("\"" "(" "[" "{")))
 
 (add-hook 'haskell-mode-hook 'dss/haskell-hook)
 
-;; Use ido-mode completion (matches anywhere, not just beginning)
-;;
-;; WARNING: This causes some versions of Emacs to fail so badly
-;; that Emacs needs to be restarted.
 (setq scion-completing-read-function 'ido-completing-read)
 
 (provide 'dss-haskell)
