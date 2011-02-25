deb171cb3e30aa978fb496fdfd4d4a226ce8bed7
Tavis Rudd <tavis@damnsimple.com>
2011-02-25 15:42:12 -0800

    elpa update

--- dss-elpa-support.el
+++ dss-elpa-support.el
@@ -1,23 +1,25 @@
-;; I keep this off for now as I don't find elpa to be complete enough for my needs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize)
+  (add-to-list 'package-archives
+                   '("technomancy" . "http://repo.technomancy.us/emacs/") t))
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;;; This was installed by package-install.el.
-;; ;;; This provides support for the package system and
-;; ;;; interfacing with ELPA, the package archive.
-;; ;;; Move this code earlier if you want to reference
-;; ;;; packages in your .emacs.
-;; (when
-;;     (load
-;;      (expand-file-name "~/.emacs.d/elpa/package.el"))
-;;   (package-initialize))
 
-
-(defun dss/init-elpa ()
-  (interactive)
-  (unless (boundp 'package-initialize)
-    (when
-        (load
-         (expand-file-name (concat dss-dotfiles-dir "elpa/package.el")))
-      (package-initialize))))
+;; (defun dss/init-elpa ()
+;;   (interactive)
+;;   (unless (boundp 'package-initialize)
+;;     (when
+;;         (load
+;;          (expand-file-name (concat dss-dotfiles-dir "elpa/package.el")))
+;;       (add-to-list 'package-archives
+;;                    '("technomancy" . "http://repo.technomancy.us/emacs/") t)
+;;       (package-initialize))))
 
 (provide 'dss-elpa-support)
