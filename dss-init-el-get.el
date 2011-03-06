b9d41572d3f16e6bb57468a1bff646844cf61cbf
Tavis Rudd <tavis@damnsimple.com>
2011-03-06 13:56:25 -0800

    miscellaneous

--- dss-init-el-get.el
+++ dss-init-el-get.el
@@ -11,11 +11,15 @@
         (:name unit-test :type emacswiki)
         rainbow-mode
         smex
-        (:name slime :type git :url "https://github.com/technomancy/slime.git" :load "slime-autoloads.el")
+        (:name slime :type git
+               ;;:url "https://github.com/technomancy/slime.git"
+               :url "http://sbcl.boinkor.net/git/slime.git"
+               :load "slime-autoloads.el")
         (:name slime-repl :type git
-               :url "https://github.com/technomancy/slime.git"
+               ;;:url "https://github.com/technomancy/slime.git"
+               :url "http://sbcl.boinkor.net/git/slime.git"
                :load "contrib/slime-repl.el")
-        ;(:name slime :type git :url "git://sbcl.boinkor.net/slime.git" :load-path ("." "./contrib") :compile nil :load "slime-autoloads.el") ; Overridden to prefer git mirror
+                                        ;(:name slime :type git :url "git://sbcl.boinkor.net/slime.git" :load-path ("." "./contrib") :compile nil :load "slime-autoloads.el") ; Overridden to prefer git mirror
 
         (:name slime-fuzzy :type http :url
                "http://elder-gods.org/~larry/repos/slime-tracker/contrib/slime-fuzzy.el")
@@ -27,9 +31,13 @@
                )
 
         durendal
+        (:name ac-dabbrev :type emacswiki)
         pymacs
         yasnippet
+        iedit
         undo-tree
+        (:name relax :type elpa)
+        coffee-mode
         (:name moz :type git :url "http://github.com/bard/mozrepl.git"
                :load "chrome/content/moz.el")
         (:name pomodoro :type emacswiki)
