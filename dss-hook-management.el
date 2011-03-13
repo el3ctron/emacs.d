3881da97596f96e9a91edab9cc07cb28d6a82f55
Tavis Rudd <tavis@damnsimple.com>
2011-03-13 08:54:21 -0700

    made this hook management more general

--- dss-hook-management.el
+++ dss-hook-management.el
@@ -1,20 +1,47 @@
-(defun dss/add-after-save-hook (fname)
-  (interactive "aWhich function: ")
-  (add-hook 'after-save-hook fname t t))
-
-(defun dss/get-after-save-hook-funcs ()
+(defun dss/get-hook-funcs (hook)
   (delq nil (mapcar
              (lambda (e) (if (symbolp e) e))
-             after-save-hook)))
+             hook)))
+
+(defun dss/get-hook-funcs-names (hook)
+  (mapcar 'symbol-name
+          (dss/get-hook-funcs
+           (if (symbolp hook)
+               (symbol-value hook)
+             hook))))
+
+(defun dss/get-all-hooks ()
+  (let (hlist (list))
+    (mapatoms (lambda (a)
+                (if (and (not (null (string-match ".*-hook"
+                                                  (symbol-name a))))
+                         (not (functionp a)))
+                    (add-to-list 'hlist a))))
+    hlist))
+
+(defun dss/remove-from-hook (hook fname &optional local)
+  (interactive
+   (let ((hook (intern (ido-completing-read
+                        "Which hook? "
+                        (mapcar #'symbol-name (dss/get-all-hooks))))))
+     (list hook
+           (ido-completing-read "Which? " (dss/get-hook-funcs-names hook))
+           local)))
+  (remove-hook hook
+               (if (stringp fname)
+                   (intern fname)
+                 fname)
+               local))
 
 (defun dss/remove-after-save-hook (fname)
   (interactive (list (ido-completing-read
                       "Which? "
-                      (mapcar 'symbol-name (dss/get-after-save-hook-funcs)))))
-  (remove-hook 'after-save-hook
-               (if (stringp fname)
-                   (intern fname)
-                 fname)
-               t))
+                      (dss/get-hook-funcs-names after-save-hook))))
+  (dss/remove-from-hook 'after-save-hook fname t))
+
+(defun dss/add-after-save-hook (fname)
+  (interactive "aWhich function: ")
+  (add-hook 'after-save-hook fname t t))
+
 
 (provide 'dss-hook-management)
