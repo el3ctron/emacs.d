bc1ec30734e34cdcabf7680a8fc5e766ceba4110
Tavis Rudd <tavis@damnsimple.com>
2011-03-17 17:57:49 -0700

    add comment

--- dss-erlang.el
+++ dss-erlang.el
@@ -1,6 +1,8 @@
+;; see http://bc.tech.coop/blog/070528.html
 (add-to-list 'load-path "~/src/distel/elisp/")
 (require 'distel)
 (distel-setup)
+
 (defun dss/erlang-mode-hook ()
   (interactive)
   (linum-mode))
