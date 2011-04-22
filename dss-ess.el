409a6935f18942708328103b09984d63cc90416b
Tavis Rudd <tavis@damnsimple.com>
2011-04-21 17:01:35 -0700

    some additional functions for evaluation and help

--- dss-ess.el
+++ dss-ess.el
@@ -18,6 +18,30 @@
   (dss/ess-end-of-buffer)
   (run-with-timer 1 nil 'dss/ess-end-of-buffer))
 
+(defun dss/ess-eval-paragraph-and-step ()
+  (interactive)
+  (mark-paragraph)
+  (setq mark-active nil)
+  (call-interactively 'dss/flash-region)
+  (call-interactively 'ess-eval-paragraph-and-step))
+
+(defun dss/ess-eval ()
+  (interactive)
+  (if mark-active
+      (call-interactively 'ess-eval-region)
+    (call-interactively 'dss/ess-eval-paragraph-and-step))
+  (run-with-timer 0.3 nil 'dss/ess-end-of-buffer)
+  (run-with-timer 1 nil 'dss/ess-end-of-buffer))
+
+(defun dss/ess-tag-it ()
+  (interactive)
+  (save-excursion
+    (k2-copy-whole-sexp)
+    (with-current-buffer "*R*"
+      (insert "it <- ")
+      (call-interactively 'yank)
+      (comint-send-input ""))))
+
 (defun dss/ess-electric-pair ()
   (interactive)
   (let (parens-require-spaces)
@@ -25,10 +49,18 @@
 
 (defun dss/ess-mode-hook ()
   (interactive)
+  (linum-mode 1)
   (dss/map-define-key ess-mode-map '("\"" "(" "[" "{") 'dss/ess-electric-pair)
   (define-key ess-mode-map (kbd "C-c C-c") #'dss/ess-load-file))
 
 (add-hook 'ess-mode-hook 'dss/ess-mode-hook)
+
+(defun dss/ess-help-mode-hook ()
+  (interactive)
+  (linum-mode 1)
+  (define-key ess-help-mode-map (kbd "M-.") #'ess-help))
+
+(add-hook 'ess-help-mode-hook 'dss/ess-help-mode-hook)
 
 (require 'ess-inf)
 (defun dss/inferior-ess-mode-hook ()
@@ -36,9 +68,9 @@
   (dss/map-define-key inferior-ess-mode-map '("\"" "(" "[" "{") 'dss/ess-electric-pair))
 (add-hook 'inferior-ess-mode-hook 'dss/inferior-ess-mode-hook)
 
-(define-key inferior-ess-mode-map (kbd "C-d")
-  (lambda ()
-    (interactive)
-    (ess-quit-r)))
+;; (define-key inferior-ess-mode-map (kbd "C-d")
+;;   (lambda ()
+;;     (interactive)
+;;     (ess-quit-r)))
 
 (provide 'dss-ess)
