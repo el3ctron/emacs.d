d203a441436bbb0f321b15c176028b0f3d02e531
Tavis Rudd <tavis@damnsimple.com>
2011-08-24 09:56:41 -0700

    - turned off follow-mode - added code from http://www.masteringemacs.org/articles/2011/07/20/searching-buffers-occur-mode/

--- dss-search-occur-etc.el
+++ dss-search-occur-etc.el
@@ -3,13 +3,14 @@
 (eval-after-load "isearch" '(require 'isearch+))
 
 ;; follow mode for output of grep and occur
-(require 'fm)
-(defun fm-start-off()
-  (fm-start)
-  (fm-toggle))
-(add-hook 'occur-mode-hook 'fm-start-off)
-;(add-hook 'ibuffer-mode-hooks 'fm-start-off)
-(add-hook 'ibuffer-occur-mode-hooks 'fm-start-off)
+;;; (require 'fm)
+;;; (defun fm-start-off()
+;;;   (fm-start)
+;;;   (fm-toggle))
+;;; (add-hook 'occur-mode-hook 'fm-start-off)
+;;; ;(add-hook 'ibuffer-mode-hooks 'fm-start-off)
+;;; (add-hook 'ibuffer-occur-mode-hooks 'fm-start-off)
+;;;
 
 ;;; isearch-forward-at-point
 ;; from http://platypope.org/blog/2007/8/5/a-compendium-of-awesomeness
@@ -32,6 +33,23 @@
   (setq isearch-string isearch-initial-string)
   (isearch-search-and-update))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; http://www.masteringemacs.org/articles/2011/07/20/searching-buffers-occur-mode/
+(defun dss/get-buffers-matching-mode (mode)
+  "Returns a list of buffers where their major-mode is equal to MODE"
+  (let ((buffer-mode-matches '()))
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (if (eq mode major-mode)
+            (add-to-list 'buffer-mode-matches buf))))
+    buffer-mode-matches))
+
+(defun dss/multi-occur-in-this-mode ()
+  "Show all lines matching REGEXP in buffers with this major mode."
+  (interactive)
+  (multi-occur
+   (get-buffers-matching-mode major-mode)
+      (car (occur-read-primary-args))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'dss-search-occur-etc)
