fdef52d92f2563139457ae587681bc7a4dba4643
Tavis Rudd <tavis@damnsimple.com>
2011-09-18 09:42:56 -0700

    rm explicit load

--- dss-vc.el
+++ dss-vc.el
@@ -13,7 +13,4 @@
   (setq ediff-split-window-function 'split-window-horizontally)
   (ediff-vc-internal "" ""))
 
-(if (file-exists-p (concat dss-vendor-dir "dvc/++build/dvc-load.el"))
-    (load-file (concat dss-vendor-dir "dvc/++build/dvc-load.el")))
-
 (provide 'dss-vc)
