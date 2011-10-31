e13126ac50cf94163fc9dd0f0dd15937a2f88152
Tavis Rudd <tavis@damnsimple.com>
2011-10-31 12:21:01 -0700

    make slime work with tramp

--- dss-slime.el
+++ dss-slime.el
@@ -1,4 +1,3 @@
-
 (require 'slime)
 (slime-setup '(slime-repl))
 (setq slime-protocol-version 'ignore)
@@ -17,6 +16,52 @@
                     ;;ac-source-dictionary
                     )))
 
+;;; http://common-lisp.net/project/slime/doc/html/Setting-up-pathname-translations.html
+;; (add-hook 'slime-connected-hook
+;;           (lambda ()
+;;             (push (list ".*"
+;;                         (lambda (filename)
+;;                           filename)
+;;                         (lambda (filename)
+;;                           filename))
+;;                   slime-filename-translations)
+;;             (push (list "li47-21"
+;;                         (lambda (filename)
+;;                           (subseq filename (length "/ssh:hunchentoot@sample.ponto-dot.com#5201:")))
+;;                         (lambda (filename)
+;;                           (concat "/ssh:hunchentoot@sample.ponto-dot.com#5201:" filename)))
+;;                   slime-filename-translations)))
+;;; /scpc:sociallinks:
+
+
+(defun slime-tramp-local-filename (f)
+  (interactive)
+  (if (file-remote-p f)
+      (tramp-file-name-localname
+       (tramp-dissect-file-name f))
+    f))
+
+(defun slime-tramp-remote-filename (f)
+  (interactive)
+  (if (file-remote-p default-directory)
+      (tramp-make-tramp-file-name
+       (tramp-file-name-method
+    (tramp-dissect-file-name default-directory))
+       (tramp-file-name-user
+    (tramp-dissect-file-name default-directory))
+       (tramp-file-name-host
+    (tramp-dissect-file-name default-directory))
+       f)
+    f))
+
+(defun slime-remote-file-name-hook ()
+  (interactive)
+    (setq slime-from-lisp-filename-function
+      'slime-tramp-remote-filename)
+    (setq slime-to-lisp-filename-function
+      'slime-tramp-local-filename))
+
+(add-hook 'slime-connected-hook 'slime-remote-file-name-hook)
 
 (defun dss/slime-hook ()
   (paredit-mode 1)
